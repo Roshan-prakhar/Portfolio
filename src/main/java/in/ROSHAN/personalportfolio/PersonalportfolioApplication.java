14218da71a1f937d5e00e5f21a66dec6a2d1fc98
Roshan_Prakhar <rtgrosh@gmail.com>
2025-07-21 01:13:32 +0530

    Portfolio

---
+++ src/main/java/in/ROSHAN/personalportfolio/PersonalportfolioApplication.java
@@ -0,0 +1,13 @@
+package in.ROSHAN.personalportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalportfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalportfolioApplication.class, args);
+	}
+
+}
